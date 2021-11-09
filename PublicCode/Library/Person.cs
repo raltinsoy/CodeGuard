9bcb130dd25ff8d47cb38d3e88470e4a04db9163
raltinsoy <ramazanaltinsoy@outlook.com>
2021-11-09 22:24:52 +0300

    referans noktası eklendi

---
+++ PublicCode/Library/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(int id, string name)
+        {
+        }
+
+        public string GetKey()
+        {
+            return "Person.GetKey.2";
+        }
+
+        private string DummyPrivateFunc()
+        {
+            return "Person.DummyPrivateFunc.2";
+        }
+
+        protected string DummyProtectedFunc()
+        {
+            return "Person.DummyProtectedFunc.2";
+        }
+    }
+}
---
+++ Test/TestConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(5, "Ramazan");
+            var key = person.GetKey();
+            var name = person.Name;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Key: " + key);
+        }
+    }
+}
